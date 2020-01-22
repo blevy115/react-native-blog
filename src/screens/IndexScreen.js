3be5549c1ae40f2af2631faa315d4c631a345a08
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-22 16:39:58 -0500

    Added delete icon and some styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
@@ -14,12 +15,35 @@ const IndexScreen = () => {
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem = {({ item }) => <Text>{item.title}</Text>}
+        renderItem = {({ item }) => {
+          return <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather
+              name='trash-2'
+              style={styles.icon}
+            />
+          </View>
+        }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    paddingVertical:10,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
