2ea3ba99453c9bc5b02cc13caa2e25480c655b9c
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-26 14:19:47 -0500

    Setup axios with ngrok to get lists of posts from json server and useEffect to call getBlogPOsts a single time

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch(action.type) {
+    case 'get_blogposts':
+      return action.payload;
     case 'add_blogpost':
       return [...state,
         {
@@ -20,6 +23,13 @@ const blogReducer = (state, action) => {
       })
     default:
       return state
+  }
+}
+
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    dispatch({type: 'get_blogposts', payload:response.data})
   }
 }
 
@@ -45,5 +55,5 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [ {title: 'Test Post', content: 'Test Content', id:1}])
+  { getBlogPosts, addBlogPost, deleteBlogPost, editBlogPost },
+  [])
